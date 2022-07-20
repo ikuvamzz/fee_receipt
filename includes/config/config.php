fad4a7b2836af68c1710b9015564d531260ca8c3
ikuvamzz <76558264+ikuvamzz@users.noreply.github.com>
2022-07-20 22:47:35 +0530

    Update config.php

--- includes/config/config.php
+++ includes/config/config.php
@@ -12,14 +12,4 @@
     //$db_con = pg_connect($db_host, $db_user, $db_pass, $db_name);
     $db_con = pg_connect( "host = $db_host port = $db_port dbname = $db_name user = $db_user password=$db_pass"  );
 
-    //test connection
-    if(mysqli_connect_error())
-        die("<div class='error'><b>Database query faild</b>" . mysqli_connect_error() . "(" . mysqli_connect_errno() . ")</div>");
-    
-    //select institute data
-    $inst_table = "SELECT * from institute";
-    $result = mysqli_query($db_con, $inst_table);
-    if(!$result){
-        die("<div class='error'><b>Oh! sorry,</b> Unable to find '<b>Institute</b>' table in database.</div>");
-    }
 ?>
