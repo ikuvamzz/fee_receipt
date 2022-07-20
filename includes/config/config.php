2e73fea77fdc93a812a69f2a465ef65aa5fb36a7
ikuvamzz <76558264+ikuvamzz@users.noreply.github.com>
2022-07-20 22:43:29 +0530

    Update config.php

--- includes/config/config.php
+++ includes/config/config.php
@@ -4,11 +4,13 @@
     session_start();
     $db_urls = parse_url(getenv('DATABASE_URL'));
     $db_host = $db_urls["host"];
+    $db_port = $db_urls["port"];
     $db_user = $db_urls["user"];
     $db_pass = $db_urls["pass"];
     $db_name = ltrim($db_urls["path"],'/');
 
-    $db_con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    //$db_con = pg_connect($db_host, $db_user, $db_pass, $db_name);
+    $db_con = pg_connect( "host = $db_host port = $db_port dbname = $db_name user = $db_user password=$db_pass"  );
 
     //test connection
     if(mysqli_connect_error())
